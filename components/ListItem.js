058b37564f6a2534c038044179517b5de8e37d38
Niklas Nilsson <48365153+Jalsson@users.noreply.github.com>
2020-08-31 18:57:13 +0300

    initial commit

--- components/ListItem.js
+++ components/ListItem.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightblue',
     flex: 1,
     flexDirection: 'row',
-    marginBottom: '5px',
+    marginBottom: 5,
   },
   imageStyle: {
     height: '100%',
