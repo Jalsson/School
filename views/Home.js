34f7641d0c2b3d22251f88ead7c831b079da5f43
Niklas Nilsson <48365153+Jalsson@users.noreply.github.com>
2020-09-04 21:44:21 +0300

    Added List to Home.js

---
+++ views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+      <SafeAreaView style={styles.container}>
+        <List />
+        <StatusBar style="auto"/>
+      </SafeAreaView>
+    );
+  };
+
+  export default Home;
