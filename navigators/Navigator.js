4dcc404c4ea2361ef15c0e517124ba267137e0a0
Niklas Nilsson <48365153+Jalsson@users.noreply.github.com>
2020-09-04 23:30:34 +0300

    Navigation ready

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import Home from '../views/Home';
+import Profile from '../views/Profile';
+import Single from '../views/Single';
+// TODO: add neccessary imports
 
-const Tab = createBottomTabNavigator();
+// add after createBottomTabNavigator
+const Stack = createStackNavigator();
+
+const TabScreen = () => {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='Profile' component={Profile} />
+      </Tab.Navigator>
+    );
+  };
+
+const StackScreen = () => {
+ return (
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={TabScreen} />
+      <Stack.Screen name='Single' component={Single} />
+    </Stack.Navigator>
+ );
+};
 
 const Navigator = () => {
-  return (
-    <NavigationContainer>
-      // TODO: Make tab navigator here
-    </NavigationContainer>
-  );
+ return (
+   <NavigationContainer>
+     <StackScreen/>
+   </NavigationContainer>
+ );
 };
 
 export default Navigator;
