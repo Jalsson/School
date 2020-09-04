648c0fe2df5de8f5288916fc34a4ca031a02fde4
Niklas Nilsson <48365153+Jalsson@users.noreply.github.com>
2020-09-04 23:36:13 +0300

    Home screen ready

--- views/Home.js
+++ views/Home.js
@@ -1,12 +1,29 @@
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
+import {StyleSheet,SafeAreaView,} from 'react-native';
+import List from '../components/List';
+import PropTypes from 'prop-types';
 
-const Home = () => {
-    return (
-      <SafeAreaView style={styles.container}>
-        <List />
-        <StatusBar style="auto"/>
-      </SafeAreaView>
-    );
-  };
 
-  export default Home;
+const Home = ({navigation}) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <List navigation={navigation} />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+  },
+});
+
+Home.propTypes = {
+  navigation: PropTypes.object,
+};
+
+
+export default Home;
