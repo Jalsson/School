bd3e7ef6328c1b5ecf5b5e8d4332230858b2df30
Niklas Nilsson <48365153+Jalsson@users.noreply.github.com>
2020-09-05 17:26:46 +0300

    Added styles and proptypes to formTextInput

--- components/FormTextInput.js
+++ components/FormTextInput.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {StyleSheet, TextInput} from 'react-native';
+import PropTypes from 'prop-types';
 
 const FormTextInput = ({style, ...otherProps}) => {
     return (
@@ -8,6 +9,18 @@ const FormTextInput = ({style, ...otherProps}) => {
         {...otherProps}
       />
     );
+  };
+
+  const styles = StyleSheet.create({
+    textInput: {
+      height: 40,
+      borderColor: '#ccc',
+      borderWidth: 1,
+    },
+  });
+
+  FormTextInput.propTypes = {
+    style: PropTypes.object,
   };
 
   export default FormTextInput;
